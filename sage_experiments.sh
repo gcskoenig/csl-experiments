2ea998d8db26b9b818d641f80ccb03fcc1352869
ChristophLuther <cph.luther@gmail.com>
2022-08-19 14:20:10 +0200

    minor changes bash script

--- sage_experiments.sh
+++ sage_experiments.sh
@@ -6,8 +6,7 @@ Rscript datagen/datagen_pcalg.R 1000000
 
 python sample_targets.py
 
-declare -a datasets=( dag_s_0.2 dag_s_0.3 dag_s_0.4 dag_sm_0.1 dag_sm_0.15 dag_sm_0.2 dag_m_0.04 dag_m_0.06 dag_m_0.08
-                    dag_l_0.02 dag_l_0.03 dag_l_0.04)
+declare -a datasets=( dag_s_0.2 dag_sm_0.1 dag_m_0.04 dag_l_0.02 )
 
 for data in "${datasets[@]}"
 do
