19bf75341fdf18ec2bfdd42b1ec4a4252586cbdd
christophluther <cph.luther@gmail.com>
2022-08-30 16:07:13 +0200

    minor changes

--- sage_experiments.sh
+++ sage_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-declare -a datasets=( dag_s_0.2 dag_sm_0.1 dag_m_0.04 dag_l_0.02 )
+declare -a datasets=( dag_s_0.2 dag_sm_0.1 dag_m_0.04 )
 
 for data in "${datasets[@]}"
 do
   echo "$data"
-  python sage_cont.py --data "$data"
+  python sage_cont.py --data "$data" --model "rf"
 done
